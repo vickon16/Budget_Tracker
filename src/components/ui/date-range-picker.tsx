28bebc535bc9007ff9db0d8ce13d8f570d965869
Nkachukwu Victor <nkachukwuvictor2016@gmail.com>
2025-08-05 20:27:34 +0100

    Updated typescript error

--- src/components/ui/date-range-picker.tsx
+++ src/components/ui/date-range-picker.tsx
@@ -98,7 +98,7 @@ export const DateRangePicker: FC<DateRangePickerProps> & {
   align = "end",
   locale = "en-US",
   showCompare = true,
-}): JSX.Element => {
+}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const [range, setRange] = useState<DateRange>({
@@ -119,8 +119,8 @@ export const DateRangePicker: FC<DateRangePickerProps> & {
   );
 
   // Refs to store the values of range and rangeCompare when the date picker is opened
-  const openedRangeRef = useRef<DateRange | undefined>();
-  const openedRangeCompareRef = useRef<DateRange | undefined>();
+  const openedRangeRef = useRef<DateRange | undefined>(undefined);
+  const openedRangeCompareRef = useRef<DateRange | undefined>(undefined);
 
   const [selectedPreset, setSelectedPreset] = useState<string | undefined>(
     undefined
@@ -299,7 +299,7 @@ export const DateRangePicker: FC<DateRangePickerProps> & {
     preset: string;
     label: string;
     isSelected: boolean;
-  }): JSX.Element => (
+  }) => (
     <Button
       className={cn(isSelected && "pointer-events-none")}
       variant="ghost"
